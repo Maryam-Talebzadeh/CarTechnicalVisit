75359dd3cfb279cfafeab578eacf6e1fcaa16ba1
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 00:30:54 -0700

    Wrote the Request Config..

---
+++ App.Infra.DB.SqlServer.EFCore/Configs/RequestAg/RequestConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using App.Domain.Core.RequestAg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DB.SqlServer.EFCore.Configs.RequestAg
+{
+    public class RequestConfiguration : IEntityTypeConfiguration<Request>
+    {
+        public void Configure(EntityTypeBuilder<Request> builder)
+        {
+
+            #region Relations
+
+            builder.HasOne(r => r.User).WithMany(u => u.Requests).HasForeignKey(r => r.UserId);
+            builder.HasOne(r => r.Car).WithOne(c => c.Requset).HasForeignKey<Request>(r => r.CarId).HasForeignKey<Car>(c => c.RequestId);
+
+            #endregion
+        }
+    }
+}
