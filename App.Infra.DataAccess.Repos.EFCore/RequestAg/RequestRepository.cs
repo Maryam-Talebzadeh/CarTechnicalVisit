30bfc7d6cfe941942f05dc20fc424a8e3daedea6
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 01:14:35 -0700

    Implemented requestRepository.

---
+++ App.Infra.DataAccess.Repos.EFCore/RequestAg/RequestRepository.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain.Core.RequestAg.Data;
+using App.Domain.Core.RequestAg.Entities;
+using App.Infra.DataAccess.Repos.EFCore.General;
+using App.Infra.DB.SqlServer.EFCore.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DataAccess.Repos.EFCore.RequestAg
+{
+    public class RequestRepository : BaseRepository<Request> ,IRequestRepository
+    {
+        private readonly CarTechnicalVisitContext _context;
+        public RequestRepository(CarTechnicalVisitContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
