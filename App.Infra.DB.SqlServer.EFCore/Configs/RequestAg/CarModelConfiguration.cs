0a5ae1c056a6ff7fffb7746e8c3df5fdba662732
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 00:38:53 -0700

    Wrote the CarModel Config..

---
+++ App.Infra.DB.SqlServer.EFCore/Configs/RequestAg/CarModelConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using App.Domain.Core.RequestAg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DB.SqlServer.EFCore.Configs.RequestAg
+{
+    public class CarModelConfiguration : IEntityTypeConfiguration<CarModel>
+    {
+        public void Configure(EntityTypeBuilder<CarModel> builder)
+        {
+            builder.Property(m => m.Title).HasMaxLength(255).IsRequired();
+        }
+    }
+}
