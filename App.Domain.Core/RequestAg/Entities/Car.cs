d9377c857cee7d4c950cba68ca5768bb11bf3c3d
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-03 23:41:41 -0700

    Added car and CarModel entity.

---
+++ App.Domain.Core/RequestAg/Entities/Car.cs
@@ -0,0 +1,30 @@
+﻿using App.Domain.Core.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.RequestAg.Entities
+{
+    public class Car : BaseEntity
+    {
+        public Car(int numberPlate, Int16 productionYear, int modelId)
+        {
+            NumberPlate = numberPlate;
+            ProductionYear = productionYear;
+            ModelId = modelId;
+        }
+
+        public int NumberPlate { get;private set; }
+        public Int16 ProductionYear { get; private set; }
+        public int ModelId { get; private set; }
+
+        void Edit(int numberPlate, Int16 productionYear, int modelId)
+        {
+            NumberPlate = numberPlate;
+            ProductionYear = productionYear;
+            ModelId = modelId;
+        }
+    }
+}
---
+++ App.Domain.Core/RequestAg/Entities/CarModel.cs
@@ -0,0 +1,24 @@
+﻿using App.Domain.Core.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.RequestAg.Entities
+{
+    public class CarModel : BaseEntity
+    {
+        public CarModel(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; private set; }
+
+        void Edit(string title)
+        {
+            Title = title;
+        }
+    }
+}
