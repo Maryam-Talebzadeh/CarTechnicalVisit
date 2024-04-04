b488cc2d133bd762d0fc000bce1d976d81ae2d48
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 00:23:27 -0700

    Wrote User Configuration with FluentApi.

---
+++ App.Infra.DB.SqlServer.EFCore/Configs/AccountAgg/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using App.Domain.Core.AccountAgg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DB.SqlServer.EFCore.Configs.AccountAgg
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.FullName).HasMaxLength(100).IsRequired();
+            builder.Property(u => u.NationalCode).IsRequired();
+            builder.Property(u => u.Address).HasMaxLength(500).IsRequired();
+
+        }
+    }
+}
