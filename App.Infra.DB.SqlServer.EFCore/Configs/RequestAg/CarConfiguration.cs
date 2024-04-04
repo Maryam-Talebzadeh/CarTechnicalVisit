ce1c9cf0d6359cd78fbf71e62d6c0d4b17693b98
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 00:35:07 -0700

    Wrote the Car configuration.

---
+++ App.Infra.DB.SqlServer.EFCore/Configs/RequestAg/CarConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using App.Domain.Core.RequestAg.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DB.SqlServer.EFCore.Configs.RequestAg
+{
+    public class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            #region Relations
+
+            builder.HasOne(c => c.Model).WithMany(m => m.Cars).HasForeignKey(c => c.ModelId);
+
+            #endregion
+        }
+    }
+}
