5fedfb36d5b909b9aa196d526ccf004563a25a66
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 00:16:03 -0700

    Implemented DbContext.

---
+++ App.Infra.DB.SqlServer.EFCore/Contexts/CarTechnicalVisitContext.cs
@@ -0,0 +1,38 @@
+﻿using App.Domain.Core.AccountAgg.Entities;
+using App.Domain.Core.RequestAg.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DB.SqlServer.EFCore.Contexts
+{
+    public class CarTechnicalVisitContext : DbContext
+    {
+        public CarTechnicalVisitContext(DbContextOptions<CarTechnicalVisitContext> options) : base(options)
+        {
+            
+        }
+
+        #region AccountAgg
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+
+        #region RequestAgg
+
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarModel> carModels { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
