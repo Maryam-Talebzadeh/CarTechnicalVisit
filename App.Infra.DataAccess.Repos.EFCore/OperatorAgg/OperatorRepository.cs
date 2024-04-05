6f2f7a79626b5425729609d02cc3275007788ee6
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-05 03:52:28 -0700

    Implemented operator repository

---
+++ App.Infra.DataAccess.Repos.EFCore/OperatorAgg/OperatorRepository.cs
@@ -0,0 +1,24 @@
+﻿using App.Domain.Core.OperatorAgg.Data;
+using App.Infra.DB.SqlServer.EFCore.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.DataAccess.Repos.EFCore.OperatorAgg
+{
+    public class OperatorRepository : IOperatorRepository
+    {
+        private readonly CarTechnicalVisitContext _context;
+
+        public OperatorRepository(CarTechnicalVisitContext context)
+        {
+            _context = context;
+        }
+        public bool Login(int personalId, string password)
+        {
+            return _context.Operators.Any(o => o.PersonalId == personalId &&  o.Password == password);
+        }
+    }
+}
