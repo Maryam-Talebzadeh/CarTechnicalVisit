4c697b2374f52ac35dfc50397c64805546ae3de4
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 23:08:26 -0700

    Added related packages.

--- CarTechnicalVisit.EndPoint.MVC/Program.cs
+++ CarTechnicalVisit.EndPoint.MVC/Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#region DbContext
+
+
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
