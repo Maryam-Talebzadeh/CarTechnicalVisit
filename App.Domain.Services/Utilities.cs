ec35ab07ff060b36df7113a654b6b67a1a5500fd
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-05 00:40:51 -0700

    Added Utilities class to use in services.

---
+++ App.Domain.Services/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Services
+{
+    public class Utilities
+    {
+        public static bool IsEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+    }
+}
