6ecafedb171d4e707e4110a625aa10451b5c9c82
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-05 04:00:12 -0700

    Added OperatorService

---
+++ App.Domain.Core/OperatorAgg/Services/IOperatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.OperatorAgg.Services
+{
+    public interface IOperatorService
+    {
+        OperationResult Login(int personalId, string password);
+    }
+}
---
+++ App.Domain.Services/OperatorAgg/OperatorService.cs
@@ -0,0 +1,31 @@
+﻿using App.Domain.Core;
+using App.Domain.Core.OperatorAgg.Data;
+using App.Domain.Core.OperatorAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Services.OperatorAgg
+{
+    public class OperatorService : IOperatorService
+    {
+        private readonly IOperatorRepository _operatorRepository;
+
+        public OperatorService(IOperatorRepository operatorRepository)
+        {
+            _operatorRepository= operatorRepository;
+        }
+
+        public OperationResult Login(int personalId, string password)
+        {
+            var result = new OperationResult();
+
+            if (_operatorRepository.Login(personalId, password))
+                return result.Succedded("با موفقیت وارد شدید.");
+
+            return result.Failed("اطلاعات ورودی درست نمیباشد");
+        }
+    }
+}
