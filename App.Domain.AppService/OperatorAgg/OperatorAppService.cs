e48079b594d717ac0fd8aac1b8f639a88834fcca
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-05 04:05:27 -0700

    Implemented operatorAppService

---
+++ App.Domain.AppService/OperatorAgg/OperatorAppService.cs
@@ -0,0 +1,27 @@
+﻿using App.Domain.Core;
+using App.Domain.Core.OperatorAgg.AppServices;
+using App.Domain.Core.OperatorAgg.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.AppService.OperatorAgg
+{
+    public class OperatorAppService : IOperatorAppService
+    {
+
+        private readonly IOperatorAppService _operatorAppService;
+
+        public OperatorAppService(IOperatorAppService operatorAppService)
+        {
+            _operatorAppService = operatorAppService;
+        }
+
+        public OperationResult Login(int personalId, string password)
+        {
+            return _operatorAppService.Login(personalId, password);
+        }
+    }
+}
---
+++ App.Domain.Core/OperatorAgg/AppServices/IOperatorAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.OperatorAgg.AppServices
+{
+    public interface IOperatorAppService
+    {
+        OperationResult Login(int personalId, string password);
+    }
+}
