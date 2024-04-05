35e6714640f8062d9e7093f34430e24d334bec62
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-05 00:17:02 -0700

    Created RequestService

---
+++ App.Domain.Services/RequestAg/RequestService.cs
@@ -0,0 +1,30 @@
+﻿
+
+using App.Domain.Core.RequestAg.DTOs;
+using App.Domain.Core.RequestAg.Services;
+
+namespace App.Domain.Services.RequestAg
+{
+    public class RequestService : IRequestService
+    {
+        public void Accept(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(RequestDTO command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RequestDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reject(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
