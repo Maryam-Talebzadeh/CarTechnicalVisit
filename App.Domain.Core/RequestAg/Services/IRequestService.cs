a12cf573c623415f921ed9f91590ef99f543d06f
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-04-04 22:44:40 -0700

    Created IRequestService

---
+++ App.Domain.Core/RequestAg/Services/IRequestService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.RequestAg.Services
+{
+    public class IRequestService
+    {
+
+    }
+}
